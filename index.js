13aa8f57ee990a88010736efe0cdcad8dc09c66b
Rafael A. R. Dias <eu@diasrafael.com.br>
2018-11-06 15:09:10 -0200

    correção bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = (_data, _quote = '"') => {
     let data = _data
 
-    if (typeof data !== 'number' && isNaN(Number(_data))) data = _quote + data + _quote
+    if (typeof data === 'string') data = _quote + data + _quote
 
     return data
 }
--- src/components/QuotesIfText/index.js
+++
@@ -1,9 +0,0 @@
-const __m__ = (_data, _quote = '"') => {
-    let data = _data
-
-    if (typeof data !== 'number' && isNaN(Number(_data))) data = _quote + data + _quote
-
-    return data
-}
-
-module.exports = __m__
